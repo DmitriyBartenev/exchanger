bf1e4a63d2dcbd1d278dd89bfd79078563a79d2b
Dmitriy <bartenevdmitriy@bk.ru>
2023-08-02 16:23:13 +0200

    Memoized rootSelector with reselect

--- src/lib/redux/slices/selectors.ts
+++ src/lib/redux/slices/selectors.ts
@@ -1,8 +1,13 @@
+import {createSelector} from 'reselect';
+
 import {RootState} from '../store';
 
-export const rootSelector = (state: RootState) => ({
-  availableCurrencies: state.availableCurrencies.currency,
-  availableCurrenciesFetchStatus: state.availableCurrencies.status,
-  minimalExchangeAmount: state.minimalExchangeAmount.minimalExchangeAmount.toString(),
-  estimatedExchangeAmount: state.estimatedExchangeAmount,
-});
+const selectRoot = (state: RootState) => state;
+
+export const rootSelector = createSelector([selectRoot], (rootState) => ({
+  availableCurrencies: rootState.availableCurrencies.currency,
+  availableCurrenciesFetchStatus: rootState.availableCurrencies.status,
+  minimalExchangeAmount: rootState.minimalExchangeAmount.minimalExchangeAmount.toString(),
+  estimatedExchangeAmount: rootState.estimatedExchangeAmount.estimatedAmount?.toString(),
+  estimatedExchangeAmountError: rootState.estimatedExchangeAmount.error,
+}));
