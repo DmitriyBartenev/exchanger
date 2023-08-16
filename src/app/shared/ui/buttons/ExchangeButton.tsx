943eb9c4c6b5c3432e144dda2eefa78c39064fc9
Dmitriy <bartenevdmitriy@bk.ru>
2023-08-16 14:40:28 +0200

    Added isDisabled button attr

--- src/app/shared/ui/buttons/ExchangeButton.tsx
+++ src/app/shared/ui/buttons/ExchangeButton.tsx
@@ -1,16 +1,29 @@
 import React from 'react';
 
+import {useAppSelector} from '~/lib/redux/hooks';
+import {rootSelector} from '~/lib/redux/slices/selectors';
+
 import {StyledButtonContainer, StyledExchangeButton} from './styles';
 
 interface ExchangeButtonProps {
   title: string;
-  type: 'button' | 'submit' | 'reset';
+  type?: 'button' | 'submit' | 'reset';
 }
 
-export const ExchangeButton: React.FC<ExchangeButtonProps> = ({title, type}) => {
+export const ExchangeButton: React.FC<ExchangeButtonProps> = ({title, type = 'button'}) => {
+  const {availableCurrencies, estimatedExchangeAmount, minimalExchangeAmount} =
+    useAppSelector(rootSelector);
+
+  const isDisabled =
+    availableCurrencies.status === 'loading' ||
+    estimatedExchangeAmount.status === 'loading' ||
+    minimalExchangeAmount.status === 'loading';
+
   return (
     <StyledButtonContainer>
-      <StyledExchangeButton type={type}>{title}</StyledExchangeButton>
+      <StyledExchangeButton type={type} disabled={isDisabled}>
+        {title}
+      </StyledExchangeButton>
     </StyledButtonContainer>
   );
 };
