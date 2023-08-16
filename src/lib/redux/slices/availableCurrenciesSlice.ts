f527a51663d1653eb9d4af54c912ac2dd1e1e096
Dmitriy <bartenevdmitriy@bk.ru>
2023-08-16 12:03:49 +0200

    Deleted setSearchValue reducer

--- src/lib/redux/slices/availableCurrenciesSlice.ts
+++ src/lib/redux/slices/availableCurrenciesSlice.ts
@@ -1,4 +1,4 @@
-import {createSlice, PayloadAction} from '@reduxjs/toolkit';
+import {createSlice} from '@reduxjs/toolkit';
 
 import {getAvailableCurrencies} from './thunks';
 import type {AvailableCurrenciesError, AvailableCurrenciesResponse} from './types';
@@ -7,30 +7,18 @@ export interface AvailableCurrenciesState {
   availableCurrencies: AvailableCurrenciesResponse[];
   error?: AvailableCurrenciesError | null;
   status: 'idle' | 'loading' | 'failed';
-  searchValue: string;
-  searchResults: AvailableCurrenciesResponse[];
 }
 
 const initialState: AvailableCurrenciesState = {
   availableCurrencies: [],
   error: null,
   status: 'idle',
-  searchValue: '',
-  searchResults: [],
 };
 
 export const availableCurrenciesSlice = createSlice({
   name: 'availableCurrencies',
   initialState,
-  reducers: {
-    setSearchValue: (state, action: PayloadAction<string>) => {
-      state.searchValue = action.payload;
-
-      state.searchResults = state.availableCurrencies.filter((currency) =>
-        currency.name.toLowerCase().includes(action.payload.toLowerCase()),
-      );
-    },
-  },
+  reducers: {},
   extraReducers: (builder) => {
     builder
       .addCase(getAvailableCurrencies.pending, (state) => {
@@ -48,5 +36,3 @@ export const availableCurrenciesSlice = createSlice({
       });
   },
 });
-
-export const {setSearchValue} = availableCurrenciesSlice.actions;
