e94644e7772d21b07aeb04036891a0d470fe123b
Dmitriy <bartenevdmitriy@bk.ru>
2023-08-16 14:43:27 +0200

    Added isDisabled attr

--- src/app/shared/ui/buttons/SwapButton.tsx
+++ src/app/shared/ui/buttons/SwapButton.tsx
@@ -1,15 +1,26 @@
+import {useAppSelector} from '~/lib/redux/hooks';
+import {rootSelector} from '~/lib/redux/slices/selectors';
+
 import {StyledSwapButton} from './styles';
 
 import {SwapIcon} from '../icons/SwapIcon';
 
 interface SwapButtonProps {
-  type: 'reset' | 'submit' | 'button';
+  type?: 'reset' | 'submit' | 'button';
   onClick: () => void;
 }
 
-export const SwapButton: React.FC<SwapButtonProps> = ({type, onClick}) => {
+export const SwapButton: React.FC<SwapButtonProps> = ({type = 'button', onClick}) => {
+  const {availableCurrencies, estimatedExchangeAmount, minimalExchangeAmount} =
+    useAppSelector(rootSelector);
+
+  const isDisabled =
+    availableCurrencies.status === 'loading' ||
+    estimatedExchangeAmount.status === 'loading' ||
+    minimalExchangeAmount.status === 'loading';
+
   return (
-    <StyledSwapButton type={type} onClick={onClick}>
+    <StyledSwapButton type={type} onClick={onClick} disabled={isDisabled}>
       <SwapIcon />
     </StyledSwapButton>
   );
