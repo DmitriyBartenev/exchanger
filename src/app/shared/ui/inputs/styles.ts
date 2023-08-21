4cc85ab7d9b75b7ce9fdb0ab0428a9127fb4dfe7
Dmitriy <bartenevdmitriy@bk.ru>
2023-08-21 19:38:10 +0200

    Removed input styles

--- src/app/shared/ui/inputs/styles.ts
+++ src/app/shared/ui/inputs/styles.ts
@@ -3,21 +3,13 @@ import {colors} from '~/app/styles/colors';
 
 export const StyledCalculatorInput = styled.input`
   width: 100%;
-  height: 50px;
+  height: 100%;
   background-color: ${colors.lightGray};
-  border: solid 1px ${colors.darkGray};
+  border: none;
   border-radius: 5px;
   color: ${colors.black};
   font-size: 16px;
   font-weight: 400;
   line-height: 23px;
   padding: 13px 0 13px 16px;
-`;
-
-export const StyledExchangeInput = styled(StyledCalculatorInput)<{
-  $showdropdown: boolean;
-  $isError: boolean;
-}>`
-  border-radius: ${({$showdropdown}) => ($showdropdown ? '5px 5px 0 0' : '5px')};
-  border-color: ${({$isError}) => ($isError ? colors.red : colors.darkGray)};
 `;
