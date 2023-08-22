55ea99e99795543affc174c85013a90029c14ece
Dmitriy <bartenevdmitriy@bk.ru>
2023-08-22 14:52:41 +0200

    Removed CloseButton

--- src/app/shared/ui/index.ts
+++ src/app/shared/ui/index.ts
@@ -1,10 +1,8 @@
-export {CloseButton} from './buttons/CloseButton';
 export {ExchangeButton} from './buttons/ExchangeButton';
 export {SelectCurrencyButton} from './buttons/SelectCurrencyButton';
 export {SwapButton} from './buttons/SwapButton';
 
 export {ArrowIcon} from './icons/ArrowIcon';
-export {CloseIcon} from './icons/CloseIcon';
 
 export {CalculatorInput} from './inputs/CalculatorInput';
 export {ExchangeInput} from './inputs/ExchangeInput';
