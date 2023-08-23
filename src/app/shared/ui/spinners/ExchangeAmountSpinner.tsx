bc04733fe6259bbdd9140c863042d12f19c6daa0
Dmitriy <bartenevdmitriy@bk.ru>
2023-08-23 13:40:16 +0200

    Removed spinner styles

--- src/app/shared/ui/spinners/ExchangeAmountSpinner.tsx
+++ src/app/shared/ui/spinners/ExchangeAmountSpinner.tsx
@@ -1,35 +1,6 @@
 import React from 'react';
-import styled, {keyframes} from 'styled-components';
-import {colors} from '~/app/styles/colors';
 
-const spinAnimation = keyframes`
-  0%, 100% {
-    transform: scale(1);
-  }
-  50% {
-    transform: scale(1.3);
-  }
-`;
-
-const SpinnerWrapper = styled.div`
-  display: inline-block;
-  height: 100%;
-  width: 100%;
-  display: flex;
-  align-items: center;
-  padding-left: 16px;
-`;
-
-const Dot = styled.div<{$mydelay: string}>`
-  display: inline-block;
-  width: 8px;
-  height: 8px;
-  border-radius: 50%;
-  background-color: ${colors.blue};
-  margin: 0 4px;
-  animation: ${spinAnimation} 1s infinite;
-  animation-delay: ${(props) => props.$mydelay || '0s'};
-`;
+import {StyledDot, StyledSpinnerWrapper} from './styles';
 
 interface ExchangeAmountSpinnerProps {
   dotCount?: number;
@@ -37,8 +8,8 @@ interface ExchangeAmountSpinnerProps {
 
 export const ExchangeAmountSpinner: React.FC<ExchangeAmountSpinnerProps> = ({dotCount = 3}) => {
   const dots = Array.from({length: dotCount}, (_, index) => (
-    <Dot key={index} $mydelay={`${index * 0.2}s`} />
+    <StyledDot key={index} $mydelay={`${index * 0.2}s`} />
   ));
 
-  return <SpinnerWrapper>{dots}</SpinnerWrapper>;
+  return <StyledSpinnerWrapper>{dots}</StyledSpinnerWrapper>;
 };
--- src/app/shared/ui/spinners/FetchCurrenciesSpinner.tsx
+++ src/app/shared/ui/spinners/FetchCurrenciesSpinner.tsx
@@ -1,20 +1,6 @@
 import React from 'react';
-import styled, {keyframes} from 'styled-components';
-import {colors} from '~/app/styles/colors';
 
-const FetchCurrenciesSpinnerAnimation = keyframes`
-    to { transform: rotate(360deg) }
-`;
-
-const StyledFetchCurrenciesSpinner = styled.div`
-  height: 20px;
-  width: 20px;
-  margin-right: 20px;
-  border-radius: 50%;
-  border: 5px solid ${colors.blue};
-  border-top-color: ${colors.darkGray};
-  animation: ${FetchCurrenciesSpinnerAnimation} 1s ease-in-out infinite;
-`;
+import {StyledFetchCurrenciesSpinner} from './styles';
 
 export const FetchCurrenciesSpinner: React.FC = () => {
   return <StyledFetchCurrenciesSpinner />;
