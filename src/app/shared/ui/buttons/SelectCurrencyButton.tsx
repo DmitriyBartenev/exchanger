abb219739c3a2ae977d414946ccf63b14703d507
Dmitriy <bartenevdmitriy@bk.ru>
2023-08-28 11:22:37 +0200

    Renamed props, removed icon via props

--- src/app/shared/ui/buttons/SelectCurrencyButton.tsx
+++ src/app/shared/ui/buttons/SelectCurrencyButton.tsx
@@ -1,53 +1,46 @@
 import Image from 'next/image';
 import React from 'react';
 
-import {useAppSelector} from '~/redux/hooks';
-import {rootSelector} from '~/redux/slices/selectors';
-
-import {FetchCurrenciesSpinner} from '~/ui';
+import {ArrowIcon, FetchCurrenciesSpinner} from '~/ui';
 
 import {StyledSelectCurrencyButton} from './styles';
 
 interface SelectCurrencyButtonProps {
   type?: 'reset' | 'submit' | 'button';
   ticker: string;
-  icon: React.ReactElement;
   image: string;
   buttonRef: React.MutableRefObject<HTMLButtonElement | null>;
   showDropdown: boolean;
+  isLoading: boolean;
+  disabled: boolean;
   onClick: () => void;
 }
 
 export const SelectCurrencyButton: React.FC<SelectCurrencyButtonProps> = ({
   type = 'button',
   ticker,
-  icon,
   image,
   buttonRef,
   showDropdown,
+  isLoading,
+  disabled,
   onClick,
-}) => {
-  const {availableCurrencies, isLoading} = useAppSelector(rootSelector);
-
-  const isCurrenciesLoading = availableCurrencies.status === 'loading';
-
-  return (
-    <StyledSelectCurrencyButton
-      onClick={onClick}
-      disabled={isLoading}
-      type={type}
-      ref={buttonRef}
-      $showDropdown={showDropdown}
-    >
-      {isCurrenciesLoading ? (
-        <FetchCurrenciesSpinner />
-      ) : (
-        <>
-          {image && <Image src={image} alt={ticker} width={20} height={20} />}
-          {ticker}
-          {icon}
-        </>
-      )}
-    </StyledSelectCurrencyButton>
-  );
-};
+}) => (
+  <StyledSelectCurrencyButton
+    onClick={onClick}
+    disabled={disabled}
+    type={type}
+    ref={buttonRef}
+    $showDropdown={showDropdown}
+  >
+    {isLoading ? (
+      <FetchCurrenciesSpinner />
+    ) : (
+      <>
+        {image && <Image src={image} alt={ticker} width={20} height={20} />}
+        {ticker}
+        <ArrowIcon />
+      </>
+    )}
+  </StyledSelectCurrencyButton>
+);
