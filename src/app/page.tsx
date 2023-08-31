8ac811f439329cb96306356b125ac455609a52d1
Dmitriy <bartenevdmitriy@bk.ru>
2023-08-31 15:35:12 +0200

    Added metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,31 @@
+import {Metadata} from 'next';
+
 import {Calculator} from './components/Calculator/Calculator';
+
+export const metadata: Metadata = {
+  title: 'Crypto Exchange',
+  description: 'Crypto Exchange Calculator',
+  applicationName: 'Crypto Exchanger',
+  keywords: ['Crypto', 'React', 'Calcualator', 'JavaScript', 'Exchange', 'Bitcoin', 'Ethereum'],
+  robots: {
+    index: false,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: false,
+      noimageindex: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
+  icons: {
+    icon: '/favicon.ico',
+    apple: '/favicon.ico',
+  },
+};
 
 export default function IndexPage() {
   return <Calculator />;
 }
-
-export const metadata = {
-  title: 'Crypto Exchange',
-};
