8e8f1f5c0c3d772fdcd59046bb0e7affc09b7501
Dmitriy <bartenevdmitriy@bk.ru>
2023-09-20 12:08:22 +0200

    Changed title with label

--- src/app/components/Calculator/ExchangeAddress.tsx
+++ src/app/components/Calculator/ExchangeAddress.tsx
@@ -2,23 +2,34 @@ import React from 'react';
 
 import {AddressInput, ExchangeButton} from '~/ui';
 
-import {StyledAddressContainer, StyledAddressInput, StyledAddressSubmit} from './styles';
+import {StyledExchangeAddress, StyledInputContainer} from './styles';
 
 interface ExchangeAddressProps {
-  disabled: boolean;
-  title: string;
+  disabledButton: boolean;
+  disabledInput: boolean;
+  label: string;
+  value: string;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  name: string;
 }
 
-export const ExchangeAddress: React.FC<ExchangeAddressProps> = ({disabled, title}) => {
-  return (
-    <StyledAddressContainer>
-      <p>{title}</p>
-      <StyledAddressSubmit>
-        <StyledAddressInput>
-          <AddressInput />
-        </StyledAddressInput>
-        <ExchangeButton title="Exchange" type="submit" disabled={disabled} />
-      </StyledAddressSubmit>
-    </StyledAddressContainer>
-  );
-};
+export const ExchangeAddress: React.FC<ExchangeAddressProps> = ({
+  disabledButton,
+  disabledInput,
+  label,
+  value,
+  onChange,
+  name,
+}) => (
+  <StyledExchangeAddress>
+    <AddressInput
+      value={value}
+      onChange={onChange}
+      placeholder="e.g., 0x742d35Cc6634C0..."
+      disabled={disabledInput}
+      name={name}
+      label={label}
+    />
+    <ExchangeButton title="Exchange" type="submit" disabled={disabledButton} />
+  </StyledExchangeAddress>
+);
