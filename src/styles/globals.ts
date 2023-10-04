2d365c68bdde886b1ad059044459db96f18b2dcb
Dmitriy <bartenevdmitriy@bk.ru>
2023-10-04 17:37:57 +0200

    Added globals styles

---
+++ src/styles/globals.ts
@@ -0,0 +1,27 @@
+import {createGlobalStyle} from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  html,
+  body {
+    height: 100%;
+    max-width: 960px;
+    margin: 0 auto;
+    padding: 0;
+    font-family: 'Roboto', sans-serif;
+    color: #282828;
+  }
+
+  #__next{
+    height: 100%;
+    width: 100%;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+`;
