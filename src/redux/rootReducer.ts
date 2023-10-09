5935312f89592139e3e7058172bd05f530a52baa
Dmitriy <bartenevdmitriy@bk.ru>
2023-10-09 16:35:14 +0200

    Added exchangeApi reducer path

--- src/redux/rootReducer.ts
+++ src/redux/rootReducer.ts
@@ -1,7 +1,9 @@
+import {exchangeApi} from './features/api/exchangeApi';
 import {estimatedExchangeAmountSlice} from './features/exchange/estimatedExchangeAmountSlice';
 import {minimalExchangeAmountSlice} from './features/exchange/minimalExchangeAmountSlice';
 
 export const rootReducer = {
   minimalExchangeAmount: minimalExchangeAmountSlice.reducer,
   estimatedExchangeAmount: estimatedExchangeAmountSlice.reducer,
+  [exchangeApi.reducerPath]: exchangeApi.reducer,
 };
