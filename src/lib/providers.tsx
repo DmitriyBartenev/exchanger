5ba3d6b9ccaaa85235fa062dcb8de13efe8cdd49
Dmitriy <bartenevdmitriy@bk.ru>
2023-10-09 16:48:24 +0200

    Removed unused redux provider

--- src/lib/providers.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-import {Provider} from 'react-redux';
-
-import store from './redux/store';
-
-export const Providers = (props: React.PropsWithChildren) => {
-  return <Provider store={store}>{props.children}</Provider>;
-};
