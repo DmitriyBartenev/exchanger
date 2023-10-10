96ddd35f888f495ba78802e2aaff81a6d3b8ff3f
Dmitriy <bartenevdmitriy@bk.ru>
2023-10-10 16:59:14 +0200

    Removed App router. Reinit app with pages router

--- src/lib/redux/createAppAsyncThunk.ts
+++
@@ -1,9 +0,0 @@
-import {createAsyncThunk} from '@reduxjs/toolkit';
-
-import type {ReduxDispatch, ReduxState} from './store';
-
-export const createAppAsyncThunk = createAsyncThunk.withTypes<{
-  state: ReduxState;
-  dispatch: ReduxDispatch;
-  rejectValue: string;
-}>();
--- src/lib/redux/rootReducer.ts
+++
@@ -1,9 +0,0 @@
-import {availableCurrenciesSlice} from './slices/availableCurrenciesSlice';
-import {estimatedExchangeAmountSlice} from './slices/estimatedExchangeAmountSlice';
-import {minimalExchangeAmountSlice} from './slices/minimalExchangeAmountSlice';
-
-export const reducer = {
-  availableCurrencies: availableCurrenciesSlice.reducer,
-  minimalExchangeAmount: minimalExchangeAmountSlice.reducer,
-  estimatedExchangeAmount: estimatedExchangeAmountSlice.reducer,
-};
